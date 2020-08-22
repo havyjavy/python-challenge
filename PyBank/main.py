7d2cfb5c98b8f08056de2d8e71cd5589d1e332ff
HavyJavy <portocarrero.javier@gmail.com>
2020-08-22 12:53:16 -0500

    updated code logic to no longer get an error

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,31 +14,32 @@ with open (file_path, 'r') as csvfile:
 
     for row in csvreader:
 
-    total_months = 0
-    Net_Change = []
-    total_net = 0
-    change_in_month = []
 
-    greatest_increase = ["", 0]
-    greatest_decrease = ["", 99999999999]
+        Net_Change = []
+        total_net = 0
+        change_in_month = []
+
+        total_months = 0
+        greatest_increase = ["", 0]
+        greatest_decrease = ["", 99999999999]
 
 
 # tracking the totals
-    total_months = total_months + 1
-    total_net = total_net + int(row[1])
+        total_months = total_months + 1
+        total_net = total_net + int(row[1])
 
 # track net changes
-    Change = int(row[1]) - previousnet
-    previousnet = int(row[1])
-
-    Net_Change = Net_Change + [Change]
+        previousnet = int(row[1])
+        Change = int(row[1]) - previousnet
+        
+        Net_Change = Net_Change + [Change]
     # Net_Change += [Change]
-    change_in_month += [row[0]]
+        change_in_month += [row[0]]
 
     #calculate greatest increase
-    if Change > greatest_increase[1]:
-        greatest_increase[0] = row[0]
-        greatest_increase[1] = Change
+        if Change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = Change
         
 
 
